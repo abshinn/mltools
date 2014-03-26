074779bdb4150281f018a7b3e41e78088ad8fe09
Adam Shinn <adam.b.shinn@gmail.com>
2014-03-25 22:07:40 -0700

    commit before push to github

--- compute.py
+++ compute.py
@@ -13,12 +13,10 @@ def cost(X, y, theta):
 def descent(X, y, theta, alpha, num_iters):
     """compute gradient descent (one variable)"""
     m = len(y)
-    J_history = np.array([0]*num_iters)
+    J_history = np.zeros(num_iters)
     for ii in range(num_iters):
         # theta -= alpha * X' * (X*theta - y) / m
         Xtheta_minus_y = X.dot(theta).sub(y.squeeze(), axis = 0)
         theta = theta.sub((alpha * X.T.dot(Xtheta_minus_y) / m).squeeze(), axis = 0)
         J_history[ii] = cost(X, y, theta)
-    return (theta, J_history[ii])
-
-
+    return (theta, J_history)
