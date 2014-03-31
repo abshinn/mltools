57223b6e7ce02e325e0aa3bc5e58e450ea1adf17
Adam Shinn <adam.b.shinn@gmail.com>
2014-03-30 21:09:42 -0700

    now uses mtcars dataset in 1D gradient descent example

--- py_ex1.py
+++ py_ex1.py
@@ -4,6 +4,7 @@
 import numpy  as np
 import pandas as pd
 import compute
+import ggplot as gg
 import matplotlib.pyplot as plt
 import pdb # debugging
 
@@ -11,11 +12,15 @@ import pdb # debugging
 np.set_printoptions(precision = 3)
 
 # use pandas to read in csv
-data = pd.read_csv('data_ex1.txt', header = None)
+#data = pd.read_csv('data_ex1.txt', header = None)
 
-# assume data is formated such that y is the last column
-y = np.matrix(data.values[:,-1]).T
-X = np.matrix(data.values[:,0:-1])
+# example for handpicking from a pandas column:
+#y = np.matrix(data.values[:,-1]).T
+#X = np.matrix(data.values[:,0:-1])
+
+# let us pick mpg to be y so that we can use regression to guess the mpg given cyl
+X = np.mat(gg.mtcars.cyl.values).T
+y = np.mat(gg.mtcars.mpg.values).T
 
 length = len(y)
 
@@ -28,7 +33,7 @@ theta = np.matrix(np.zeros(X.shape[1])).T
 # ----------- Gradient Descent ------------
 # initialize gradient descent parameters
 iterations = 1500
-alpha = 0.01
+alpha = 0.03
 
 # compute initial cost
 print("Initial cost: J = {:.3f}".format(compute.cost(X, y, theta)))
@@ -39,6 +44,10 @@ print("Theta found using gradient decent: {}".format(theta.T))
 
 # compute final cost
 print("Final cost: J = {:.3f}".format(J_history[-1]))
+
+# now, let us see how well the learning algorithm did
+cyl4 = np.mat("[1 4]") * theta
+print("MPG for 4 cylinders: {}".format(cyl4))
 
 # ----------- Plots -----------
 # matplotlib line and fit
@@ -55,3 +64,11 @@ plt.ylabel("J")
 plt.title("alpha = {}".format(alpha))
 
 plt.show()
+
+# use debug tools to explore variables and plots before script terminates
+pdb.set_trace()
+
+# note:
+# because of the discrete nature of the X variable, cyl, this is a crude
+# regression example... the next step up should include the displ variable
+# in a multi-variable regression in order to make better predictions
