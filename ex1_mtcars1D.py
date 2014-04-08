fixed descent in ex1 by normalizing X; fixed plot spacing in ex2

ex1_mtcars1D.py
@@ -19,7 +19,10 @@ y = np.mat(mtcars.mpg.values, dtype = float).T
 # add x2, the square of x1
 #X = mltools.addxsquare(X, 0)
 
-# add x0, all set to 1
+# normalize disp
+X, mu, sigma = mltools.featureNormalize(X)
+
+# add x0
 X = mltools.addx0(X)
 
 # initialize fitting parameters, array of 0's
@@ -27,28 +30,29 @@ theta = np.mat(np.zeros(X.shape[1])).T
 
 # ----------- Gradient Descent ------------
 # initialize gradient descent parameters
-iterations = 100
-alpha = 0.0000003
+iterations = 20 
+alpha = 0.3
+lreg = 0.0
 
 # compute initial cost
-print("Initial cost: J = {:.3f}".format(mltools.cost(X, y, theta)))
+print("Initial cost: J = {:.3f}".format(mltools.cost(X, y, theta, lreg = lreg)))
 
 # compute gradient descent
-theta, J_history = mltools.descent(X, y, theta, alpha, iterations)
+theta, J_history = mltools.descent(X, y, theta, alpha, iterations, lreg = lreg)
 print("Theta found using gradient decent: {}".format(theta.T))
 
 # display final cost
 print("Final cost: J = {:.3f}".format(J_history[-1]))
 
 # normal equation
-theta_NEq = mltools.normalEqn(X, y, lreg = 0.)
+theta_NEq = mltools.normalEqn(X, y)
 J_final = mltools.cost(X, y, theta_NEq)
 print("Cost, theta found using normal equation: {:.3f}, {}".format(J_final, theta_NEq.T))
 
 # now, let us see how well the learning algorithm did
-print("MPG for a disp of {}:  {}".format( 80, np.mat("[1  80]")*theta_NEq))
-print("MPG for a disp of {}:  {}".format(250, np.mat("[1 250]")*theta_NEq))
-print("MPG for a disp of {}:  {}".format(400, np.mat("[1 400]")*theta_NEq))
+print("MPG for a disp of {}:  {}".format( 80, np.mat([1, ( 80 - mu)/sigma])*theta_NEq))
+print("MPG for a disp of {}:  {}".format(250, np.mat([1, (250 - mu)/sigma])*theta_NEq))
+print("MPG for a disp of {}:  {}".format(400, np.mat([1, (400 - mu)/sigma])*theta_NEq))
 
 # ----------- Plots -----------
 # scatter and best fit
@@ -70,8 +74,8 @@ fig.set_tight_layout(True)
 
 # cost-space contour and surface plots
 # intialize thetas
-theta0_vals = np.linspace(-10, 35, 100)
-theta1_vals = np.linspace(-0.5, 0.5, 100)
+theta0_vals = np.linspace(10, 30, 100)
+theta1_vals = np.linspace(-15, 5, 100)
 
 # intialize J values
 J_vals = np.zeros((len(theta0_vals), len(theta1_vals)))
@@ -87,7 +91,7 @@ J_vals = J_vals.T
 
 # contour plot
 plt.figure()
-ctr = plt.contour(theta0_vals, theta1_vals, J_vals, np.logspace(-2, 4, 10))
+ctr = plt.contour(theta0_vals, theta1_vals, J_vals, np.logspace(-1, 2, 10))
 plt.clabel(ctr, inline = 1, fontsize = 10)
 plt.plot(theta_NEq[0,0], theta_NEq[1,0], 'rx', ms = 10, mew = 2)
 plt.plot(theta[0,0], theta[1,0], 'bo')
@@ -105,8 +109,9 @@ surf = ax.plot_surface(theta0_vals, theta1_vals, J_vals, cmap = cm.coolwarm, ant
 # show plots
 plt.show()
 
-# use debug tools to explore variables and plots before script terminates
+# use debug tools to explore variables before script terminates
 #pdb.set_trace()
 
 # Discussion
-#   Gadient descent faile to find the local optimum... perhaps feature scaling could come to the rescue?
+#   Interestingly, without featrure normalization, gradient descent fails to descend in the 
+#   direction of the optimal theta0.

ex2_mtcars2D.py
@@ -21,7 +21,7 @@ y = np.mat(mtcars.mpg.values, dtype = float).T
 # normalize features
 X, mu, sigma = mltools.featureNormalize(X)
 
-# add x0, all set to 1
+# add x0
 X = mltools.addx0(X)
 
 # initialize fitting parameters, array of 0's
@@ -45,7 +45,7 @@ J_final = mltools.cost(X, y, theta_NEq)
 print("Cost, theta found using normal equation: {:.3f}, {}".format(J_final, theta_NEq.T))
 
 # estimate mpg using computed gradient descent parameters
-predict = (np.mat("[4.0 80.0]") - mu) / sigma
+predict = (np.mat([4.0, 80.0]) - mu) / sigma
 predict = np.c_[ 1.0, predict ] * theta_NEq
 print("MPG for 4 cylinders, disp of 80: {}".format(predict))
 
@@ -74,7 +74,11 @@ ax3.plot(J_history)
 ax3.set_ylabel("J")
 ax3.set_title("alpha = {}".format(alpha))
 
+fig.set_tight_layout(True)
 plt.show()
 
 # use debug tools to explore variables and plots before script terminates
-pdb.set_trace()
+#pdb.set_trace()
+
+# Discussion
+#   This would be a better logistic regression example, predicting cyl from disp and mpg.

mltools.py
@@ -19,11 +19,12 @@ def polyfeatures(X1, X2, degree = 6):
             X_out = np.c[  X_out, np.multiply( np.power(X1,ii-jj), np.power(X2,jj) )  ]
     return X_out
 
-def cost(X, y, theta):
+def cost(X, y, theta, lreg = 0.):
     """compute linear regression cost function (one variable)"""
     # J = (X*theta - y)' * (X*theta - y) / 2*m
     m = len(y)
-    J = (X*theta - y).T * (X*theta - y) / (2*m)
+    #J = ( (X*theta - y).T * (X*theta - y) ) / (2*m)
+    J = ( (X*theta - y).T * (X*theta - y) + lreg*(theta.T * theta) ) / (2.*m)
     return J[0,0] # return single value
 
 def descent(X, y, theta, alpha, num_iters, lreg = 0.):
@@ -32,11 +33,15 @@ def descent(X, y, theta, alpha, num_iters, lreg = 0.):
     J_history = np.zeros(num_iters)
     for ii in range(num_iters):
         # matlab: theta -= alpha * X' * (X*theta - y) / m
+        # linear regularization TURNED ON 
+        grad = theta - alpha * ( X.T * (X*theta - y) ) / m
+        temp = theta 
+        temp[0] = 0.
+        theta = grad + alpha*lreg*temp/m
         # linear regularization TURNED OFF
-        #theta[0] = theta[0] - alpha*( X[:,0].T * (X[:,0] * theta[0] - y) )/m
-        #theta[1:] = theta[1:]*(1.0 - alpha*lreg/m) - alpha*( X[:,1:].T * (X[:,1:] * theta[1:] - y) )/m
-        theta = theta - alpha * ( X.T * (X*theta - y) ) / m
+        #theta = theta - alpha * ( X.T * (X*theta - y) ) / m
         J_history[ii] = cost(X, y, theta)
+        #pdb.set_trace()
     return (theta, J_history)
 
 def featureNormalize(X):